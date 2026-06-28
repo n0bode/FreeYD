8f39a4812ed1c9f8d747eea7f1606d38cb3d4f85
Paulo Camacan <paulodonutduck@gmail.com>
2026-06-28 02:13:59 -0300

    add: zero domain value

--- src/core/domains/item.zig
+++ src/core/domains/item.zig
@@ -1,9 +1,22 @@
+const std = @import("std");
+
 pub const Item = extern struct {
     itemID: i16,
     effect: [3]IValue,
+
+    pub fn zero() Item {
+        return Item{
+            .itemID = 0,
+            .effect = [_]IValue{.zero()} ** 3,
+        };
+    }
 };
 
 pub const IValue = extern struct {
     index: u8,
     value: u8,
+
+    pub fn zero() IValue {
+        return .{ .index = 0, .value = 0 };
+    }
 };

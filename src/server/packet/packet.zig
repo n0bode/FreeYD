e4ac1f568397279b017627ea5a98c9a99da46da1
Paulo Camacan <paulodonutduck@gmail.com>
2026-06-28 14:51:23 -0300

    fix: remove sentinel for username and password

--- src/server/packet/packet.zig
+++ src/server/packet/packet.zig
@@ -4,6 +4,9 @@ pub const Opcode = struct {
     pub const LOGIN = 0x020D;
     pub const TEXTMESSAGE = 0x0101;
     pub const CHARLIST = 0x010E;
+    pub const PING = 0x03A0;
+    pub const PIN = 0x0FDE;
+    pub const PINFAIL = 0x0FDF;
 };
 
 pub const Verifier = extern struct {
@@ -26,12 +29,21 @@ pub const PacketTextMessage = extern struct {
 
 pub const PacketLogin = extern struct {
     header: Header,
-    username: [16:0]u8,
-    password: [12:0]u8,
+    username: [16]u8,
+    password: [12]u8,
     version: i32,
     none: i32,
     keys: [16]u8,
     ipaddress: [16]u8,
+};
+
+pub const PacketPing = Header;
+pub const PacketSignal = Header;
+
+pub const PacketPin = extern struct {
+    header: Header,
+    numeric: [6]u8,
+    _unknown: [10]u8,
 };
 
 pub const PacketCharList = extern struct {
@@ -49,10 +61,14 @@ pub const PacketCharList = extern struct {
 pub const OpcodeRecv = enum(u16) {
     unknown,
     login = Opcode.LOGIN,
+    ping = Opcode.PING,
+    pin = Opcode.PIN,
 
     pub fn parse(code: u16) OpcodeRecv {
         return switch (code) {
             Opcode.LOGIN => .login,
+            Opcode.PING => .ping,
+            Opcode.PIN => .pin,
             else => .unknown,
         };
     }
@@ -71,11 +87,13 @@ pub const PacketOpcode = enum(u16) {
     login = Opcode.LOGIN,
     textmessage = Opcode.TEXTMESSAGE,
     charlist = Opcode.CHARLIST,
+    ping = Opcode.PING,
 
     pub fn parse(code: u16) PacketOpcode {
         return switch (code) {
             Opcode.LOGIN => PacketOpcode.login,
             Opcode.TEXTMESSAGE => PacketOpcode.textmessage,
+            Opcode.PING => PacketOpcode.ping,
             else => .unknown,
         };
     }
@@ -84,4 +102,6 @@ pub const PacketOpcode = enum(u16) {
 pub const Packet = union(OpcodeRecv) {
     unknown: Header,
     login: PacketLogin,
+    ping: PacketPing,
+    pin: PacketPin,
 };

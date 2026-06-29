8d244f03c39196fe869caf8d15b0aa1616195377
Paulo Camacan <paulodonutduck@gmail.com>
2026-06-29 17:11:08 -0300

    add: struct for character

--- src/core/domains/char.zig
+++ src/core/domains/char.zig
@@ -1,5 +1,46 @@
 const Item = @import("item.zig").Item;
 
+pub const Character = extern struct {
+    name: [16]u8,
+    clan: u8,
+
+    merchant: u8,
+    guild: u16,
+    class: u8,
+    rsv: u16,
+    quest: u8,
+
+    gold: i32,
+    exp: i64,
+
+    positionX: i16,
+    positionY: i16,
+
+    stats: CharStat,
+    currentStats: CharStat,
+
+    equipaments: [16]Item,
+    carry: [16]Item,
+
+    skills: u64,
+    magic: u32,
+
+    statsBonus: u16,
+    specialsBonus: u16,
+    skillsBonus: u16,
+
+    criticRate: u8,
+    saveMana: u8,
+
+    skillBar: [4]u8,
+    guildLevel: u8,
+
+    regenHp: u16,
+    regenMp: u16,
+
+    resists: [4]i8, // fire ice, element
+};
+
 pub const CharStat = extern struct {
     level: u16,
     defense: i16,
@@ -10,29 +51,15 @@ pub const CharStat = extern struct {
     speed: u8,
     chaosRate: u8,
 
-    max_hp: u16,
-    max_mp: u16,
-    current_hp: u16,
-    current_mp: u16,
+    maxHp: u16,
+    maxMp: u16,
+    currentHp: u16,
+    currentMp: u16,
 
     str: i16,
     int: i16,
     dex: i16,
     con: i16,
 
-    w_master: u8,
-    f_master: u8,
-    s_master: u8,
-    t_master: u8,
-};
-
-pub const CharacterList = extern struct {
-    positionX: [4]i16,
-    positionY: [4]i16,
-    name: [4][16]u8,
-    stats: [4]CharStat,
-    inventory: [4][16]Item,
-    guild: [4]u16,
-    gold: [4]i32,
-    exp: [4]u32,
+    specials: [4]i16,
 };

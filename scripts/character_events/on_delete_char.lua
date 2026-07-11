bef952f5afcf82bb6acafcfbace9dd17e0fd92d9
Paulo Camacan <paulodonutduck@gmail.com>
2026-07-10 21:52:03 -0300

    Create on_delete_char.lua

---
+++ scripts/character_events/on_delete_char.lua
@@ -0,0 +1,40 @@
+local server = require("server")
+local logger = require("logger")
+
+server:on("on_delete_char", function(peer, req)
+    local db = server:get_database()
+    if db == nil then
+        peer:send_text("ops.. try again soon")
+        return false
+    end
+
+    local account = peer.account
+    if account == nil then
+        peer:send_text("username or password is invalid")
+        return false
+    end
+
+    if account.password ~= req.password then
+        peer:send_text("password not matched")
+        return false
+    end
+
+    local char = account.characters[req.slot]
+    if char.name == "" then
+        return true
+    end
+
+    if char.name ~= req.name then
+        logger:error("name character incorrect")
+        peer:send_text("failed to delete, try again soon")
+        return false
+    end
+
+    char.name = ""
+    local updated = account.characters[req.slot]
+
+    logger:info("char nome " .. char.name .. " db = " .. updated.name)
+    account:save(db)
+    peer:send_text("character deleted successfully")
+    return true
+end)
